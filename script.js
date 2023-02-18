195c5a4e1c2ca327971bb17327eb659808d5aa70
Fomin <fomin-goshenka@internet.ru>
2023-02-18 15:34:47 +0300

    LP3

---
+++ script.js
@@ -0,0 +1,25 @@
+$('.section3_cont').slick({
+    centerMode: false,
+    centerPadding: '150px',
+    slidesToShow: 4,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
